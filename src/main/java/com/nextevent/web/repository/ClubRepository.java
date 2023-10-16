94b7a7483c9e4031d61dda1204c1452de98f227f
Denison Kolling <denisonkolling@gmail.com>
2023-10-16 16:01:09 -0300

    feat: add club model, repository, service, controller

---
+++ src/main/java/com/nextevent/web/repository/ClubRepository.java
@@ -0,0 +1,12 @@
+package com.nextevent.web.repository;
+
+import com.nextevent.web.models.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+    Optional<Club> findByTitle(String url);
+
+}
---
+++ src/main/java/com/nextevent/web/service/ClubService.java
@@ -0,0 +1,11 @@
+package com.nextevent.web.service;
+
+import com.nextevent.web.dto.ClubDto;
+
+import java.util.List;
+
+public interface ClubService {
+
+    List<ClubDto> findAllClubs();
+
+}
